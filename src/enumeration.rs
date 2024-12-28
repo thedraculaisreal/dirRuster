32d5a7011502f995a0b8121b07d73d93256ee87a
thedraculaisreal <jakejackson2546@gmail.com>
2024-12-28 21:17:47 +0000

    extension/directory_crawling

--- src/enumeration.rs
+++ src/enumeration.rs
@@ -1,13 +1,28 @@
 use reqwest;
 
-pub async fn directories(wordlist: &String, url: &String) {
+pub async fn directories(wordlist: &String, url: &String, extension: String) {
     let http = String::from("http://");
+    let mut crawl_urls: Vec<String> = Vec::new();
+    let mut ext = String::new();
+    match extension.as_str() {
+	".php" => ext = ".php".to_string(),
+	".html" => ext = ".html".to_string(),
+	".txt" => ext = ".txt".to_string(),
+	_ => ext = "".to_string()
+    }
     for line in wordlist.lines() {
-	let result = reqwest::get(http.clone() + &url.clone() + line).await.expect("failed to issue get request");
+	let result = reqwest::get(http.clone() + &url.clone() + line + &ext.clone()).await.expect("failed to issue get request");
 	let status_code = result.status();
 	if status_code == 200 {
-	    println!("http://{url}{line}: Status_Code:{status_code}");
+	    println!("http://{url}{line}{ext}: Status_Code:{status_code}");
+	    if ext.len() > 2 {
+		continue;
+	    }
+	    crawl_urls.push(http.clone() + &url.clone() + line);
 	}
+    }
+    for url in crawl_urls {
+	println!("{url}");
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,22 @@ mod enumeration;
 async fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 4 {
-	println!("<executable> <url> <path-to-wordlist> <options>");
+	println!("<executable> <url> <path-to-wordlist> <options> <extension>");
 	return Ok(())
     }
     let url = args[1].clone();
     let wordlist = args[2].clone();
     let option = args[3].clone(); // dir sub/vhost
+    let mut extension = String::from("");
+    if args.len() == 5 {
+	extension = args[4].clone();
+    }
     let mut file = File::open(wordlist).expect("file not found");
     let mut wordlist_contents = String::new();
     file.read_to_string(&mut wordlist_contents).expect("Faield to read file into buffer");
 
     if option == "dir" {
-	enumeration::directories(&wordlist_contents, &url).await;
+	enumeration::directories(&wordlist_contents, &url, extension).await;
     }
     if option == "sub" {
 	enumeration::sub_domains(&wordlist_contents, &url).await;
